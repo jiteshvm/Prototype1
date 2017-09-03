0689b38501b72aa15c31a170abbd2a14fa5b8ca2
Jitesh <jiteshvm@gmail.com>
2017-09-03 00:03:20 -0700

    triangles disappear when caught

---
+++ Assets/Scripts/CircleBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CircleBehavior : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("OnTriggerEnter2D : " + collision.gameObject.name);
+        PoolManager.Instance.Despawn(collision.gameObject);
+
+    }
+}
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -24,7 +24,7 @@ public class EnemySpawner : Singleton<EnemySpawner> {
         {
 			Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 10));
             PoolManager.Instance.Spawn(EnemyPrefab, screenPosition, Quaternion.AngleAxis(Random.Range(0, 360), Vector3.forward));
-			yield return new WaitForSeconds(Random.Range(2.0f, 5.0f));    
+			yield return new WaitForSeconds(Random.Range(0.2f, 2.0f));    
         }		
     }
 
